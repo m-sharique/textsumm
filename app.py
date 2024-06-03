eb549bd3e4bf96766ee7abf8954c16c7223ad975
M Sharique Siddiqui <157228213+m-sharique@users.noreply.github.com>
2024-06-03 21:59:01 +0530

    Commented the code for evaluation of model

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
-from rouge_score import rouge_scorer
+# from rouge_score import rouge_scorer
 import joblib
 
 # Download necessary NLTK data
@@ -31,20 +31,20 @@ def summarize(text, vectorizer):
     summary = ' '.join([sentences[i] for i in top_sentence_indices])
     return summary
 
-# # Function to evaluate the summarization model using ROUGE scores
-def evaluate_model(articles, summaries, vectorizer):
-    scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
-    scores = {'rouge1': [], 'rouge2': [], 'rougeL': []}
+# Function to evaluate the summarization model using ROUGE scores
+# def evaluate_model(articles, summaries, vectorizer):
+#     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
+#     scores = {'rouge1': [], 'rouge2': [], 'rougeL': []}
     
-    for article, reference_summary in zip(articles, summaries):
-        generated_summary = summarize(article, vectorizer)
-        score = scorer.score(reference_summary, generated_summary)
-        scores['rouge1'].append(score['rouge1'].fmeasure)
-        scores['rouge2'].append(score['rouge2'].fmeasure)
-        scores['rougeL'].append(score['rougeL'].fmeasure)
+#     for article, reference_summary in zip(articles, summaries):
+#         generated_summary = summarize(article, vectorizer)
+#         score = scorer.score(reference_summary, generated_summary)
+#         scores['rouge1'].append(score['rouge1'].fmeasure)
+#         scores['rouge2'].append(score['rouge2'].fmeasure)
+#         scores['rougeL'].append(score['rougeL'].fmeasure)
     
-    average_scores = {key: np.mean(value) for key, value in scores.items()}
-    return average_scores
+#     average_scores = {key: np.mean(value) for key, value in scores.items()}
+#     return average_scores
 
 # Streamlit app interface
 st.title("Text Summarization App")
@@ -56,9 +56,9 @@ if st.button("Summarize"):
     st.write("Summary:")
     st.write(summary)
 
-if st.button("Evaluate Model"):
-    average_scores = evaluate_model(articles, summaries, vectorizer)
-    st.write("Model Evaluation Scores:")
-    st.write(f"ROUGE-1 Score: {average_scores['rouge1']:.4f}")
-    st.write(f"ROUGE-2 Score: {average_scores['rouge2']:.4f}")
-    st.write(f"ROUGE-L Score: {average_scores['rougeL']:.4f}")
+# if st.button("Evaluate Model"):
+#     average_scores = evaluate_model(articles, summaries, vectorizer)
+#     st.write("Model Evaluation Scores:")
+#     st.write(f"ROUGE-1 Score: {average_scores['rouge1']:.4f}")
+#     st.write(f"ROUGE-2 Score: {average_scores['rouge2']:.4f}")
+#     st.write(f"ROUGE-L Score: {average_scores['rougeL']:.4f}")
